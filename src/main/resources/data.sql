c33e18201d59029e45362b2a413e681587cc6189
Methevas <xavier.maurice.mx@gmail.com>
2024-05-01 19:58:18 +0200

    feat(): Ajout de + de solutions pour tester la pagination du FRONT (<10)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,14 @@ CREATE TABLE solutions
 INSERT INTO solutions (numerotation) VALUES ('1,2,3,4,5,6,7,8,9');
 INSERT INTO solutions (numerotation) VALUES ('1,6,5,9,3,2,7,8,4');
 INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
+INSERT INTO solutions (numerotation) VALUES ('9,2,3,8,7,6,5,4,1');
